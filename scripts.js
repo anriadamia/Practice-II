2705adf5dff2c53db3b44c4d74f178b3782f3e73
anriadamia <anriadamia2000@gmail.com>
2022-10-21 08:31:47 +0400

    Practice II

---
+++ scripts.js
@@ -0,0 +1,65 @@
+// function sum(...numbers) {
+//   let numberSum = 0;
+//   for (let positiveNumbers of numbers) {
+//     if (positiveNumbers > 0) {
+//       numberSum += positiveNumbers;
+//     }
+//   }
+//   return numberSum;
+// }
+// let results = sum(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+// console.log(results);
+
+// function sum(...numbers) {
+//   let numberSum = 0;
+//   for (let items of numbers) {
+//     numberSum += items;
+//   }
+//   return numberSum;
+// }
+// let results = sum(10, 50, 6, 7, 8, 11, 6, 3, 9);
+// console.log(results);
+
+// let user = {
+//   firstname: "giorgi",
+//   lastname: "saakadze",
+//   age: 32,
+//   isloggedin: true,
+// };
+// if (user.isloggedin == true) {
+//   console.log(`${user.firstname} ${user.lastname}`);
+// } else {
+//   console.log("false");
+// }
+
+// let array = [1, 2, 4, 10, 34, 5, 7, 87];
+// for (items of array) {
+//   if (items > 0 && items < 10) {
+//     console.log(items);
+//   }
+// }
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (items of numbers) {
+//   if (items == 5) break;
+//   console.log(items);
+// }
+
+// let array = ["html", "css", "python", "javascript", "bootstrap"];
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i].length > 4 && array[i].includes("av")) {
+//     console.log(array[i]);
+//   }
+// }
+
+// let array = ["html", "css", "python", "javascript", "bootstrap"];
+// for (item of array) {
+//   if (item.length > 4 && item.includes("av")) {
+//     console.log(item);
+//   }
+// }
+
+// let array = ["html", "css", "python", "javascript", "bootstrap"];
+// array.forEach(function (item) {
+//   if (item.length > 4 && item.includes("av")) console.log(item);
+// });
